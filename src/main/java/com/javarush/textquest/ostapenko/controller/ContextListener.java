08edaac2214badae163cbe401bb3492b377c71b9
Darfresor <vectnik@gmail.com>
2025-10-26 10:37:00 +0300

    добавлена загрузка сервиса при запуске и в ней считывание json данных

---
+++ src/main/java/com/javarush/textquest/ostapenko/controller/ContextListener.java
@@ -0,0 +1,15 @@
+package com.javarush.textquest.ostapenko.controller;
+
+import com.javarush.textquest.ostapenko.model.QuestService;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        QuestService questService = QuestService.getInstance();
+        System.out.println("QuestService initialize with start application");
+    }
+}
--- src/main/java/com/javarush/textquest/ostapenko/model/QuestService.java
+++ src/main/java/com/javarush/textquest/ostapenko/model/QuestService.java
@@ -1,4 +1,50 @@
 package com.javarush.textquest.ostapenko.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javarush.textquest.ostapenko.model.entity.QuestCard;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestService {
+    private List<QuestCard> quests = new ArrayList<>();
+
+    private static final QuestService INSTANCE = new QuestService();
+
+    private QuestService() {
+        loadQuests();
+    }
+
+    private void loadQuests() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream inputStream = getClass().getClassLoader()
+                    .getResourceAsStream("data/quests/list.json");
+
+            if (inputStream != null) {
+                this.quests = mapper.readValue(inputStream,
+                        new TypeReference<List<QuestCard>>() {});
+                System.out.println("Loaded quests: " + quests.size());
+            }
+        } catch (Exception e) {
+            System.err.println("error load quests: " + e.getMessage());
+        }
+    }
+
+    public List<QuestCard> getAllQuests() {
+        return new ArrayList<>(quests);
+    }
+
+    public QuestCard getQuestById(Long id) {
+        return quests.stream()
+                .filter(q -> q.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static QuestService getInstance() {
+        return INSTANCE;
+    }
 }
